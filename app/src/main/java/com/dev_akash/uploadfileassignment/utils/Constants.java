5ea4260e76d3692233fd4eb07dc44d732cbda9f3
akashkumarprajapat <rkpsx7@gmail.com>
2023-05-16 13:51:56 +0530

    updated token

--- app/src/main/java/com/dev_akash/uploadfileassignment/utils/Constants.java
+++ app/src/main/java/com/dev_akash/uploadfileassignment/utils/Constants.java
@@ -2,7 +2,7 @@ package com.dev_akash.uploadfileassignment.utils;
 
 public class Constants {
 
-    public static final String ACCESS_TOKEN = "sl.Befp-wXNPsmC94vLUK8DIenwEKAQco2pzfuyG_uzfD7m3LwKiYUH9fv7iLbGhv5h2Yu2j13HJ7_byGZRdtJzckr3XvJVlHw65wDtCohX-BZI7S14IA9xQPaNQxcndNq0RSyA6q9-";
+//    public static final String ACCESS_TOKEN = "sl.Befp-wXNPsmC94vLUK8DIenwEKAQco2pzfuyG_uzfD7m3LwKiYUH9fv7iLbGhv5h2Yu2j13HJ7_byGZRdtJzckr3XvJVlHw65wDtCohX-BZI7S14IA9xQPaNQxcndNq0RSyA6q9-";
 
     public static final String[] ACCEPTED_MIME_TYPES = {
             "application/pdf",
